9f984abd3155e6b9fc9aa8a13c041dedb7670296
nileshmaurya1403 <computechcomputer01@gmail.com>
2020-08-10 07:17:28 +0530

    beach-resort-app

---
+++ src/Contentful.js
@@ -0,0 +1,13 @@
+import { createClient } from 'contentful'
+
+
+const contentful = require("contentful");
+
+export default contentful.createClient({
+
+  space:process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN
+
+})
+
+
